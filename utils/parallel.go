e7432cc443161c16272b9b189247aa45e0068994
somebodyawesome-dev <touil@brainstack.net>
2024-08-01 16:14:32 +0200

    refactored parallel module

--- utils/parallel.go
+++ utils/parallel.go
@@ -7,23 +7,25 @@ import (
 	"sync"
 )
 
-
 func ParallelImageProcess(size image.Point, f func(x int, y int)) {
 	procs := runtime.GOMAXPROCS(0)
+	xStep := int(math.Floor(float64(size.X) / float64(procs)))
+	yStep := int(math.Floor(float64(size.Y) / float64(procs)))
+
 	var waitGroup sync.WaitGroup
 	for i := 0; i < procs; i++ {
-		startX := i * int(math.Floor(float64(size.X)/float64(procs)))
+		startX := i * xStep
 		var endX int
 		if i < procs-1 {
-			endX = (i + 1) * int(math.Floor(float64(size.X)/float64(procs)))
+			endX = (i + 1) * xStep
 		} else {
 			endX = size.X
 		}
 		for j := 0; j < procs; j++ {
-			startY := j * int(math.Floor(float64(size.Y)/float64(procs)))
+			startY := j * yStep
 			var endY int
 			if j < procs-1 {
-				endY = (j + 1) * int(math.Floor(float64(size.Y)/float64(procs)))
+				endY = (j + 1) * yStep
 			} else {
 				endY = size.Y
 			}
